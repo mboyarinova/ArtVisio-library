0598141b7a6e0ef2850b9bf0d91972a9206f1081
mboyarinova <44038937+mboyarinova@users.noreply.github.com>
2020-10-27 17:51:00 +0300

    omg i think it's working

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 use App\Entity\Book;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class LibraryController extends AbstractController {
     /**
@@ -21,37 +24,78 @@ class LibraryController extends AbstractController {
 
     /**
      * @Route("/book/new", name="add_book")
+     * Method({"GET", "POST"})
      */
-    public function addBook(): Response {
-      return $this->render('library/add.html.twig');
+    public function addBook(Request $request): Response {
+
+      $book = new Book();
+      $form = $this->createFormBuilder($book)
+        ->add('title', TextType::class, array(
+          'label' => 'Название',
+          'attr' => array('class' => 'form-control')))
+        ->add('Author', TextType::class, array(
+          'label' => 'Автор',
+          'attr' => array('class' => 'form-control')))
+        ->add('Year',  TextType::class, array(
+          'label' => 'Год',
+          'attr' => array('class' => 'form-control')))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Сохранить',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+        ))
+        ->getForm();
+
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()) {
+        $book = $form->getData();
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($book);
+        $entityManager->flush();
+        return $this->redirectToRoute('book_list');
+      }
+
+      return $this->render('library/add.html.twig',
+                            array('form' => $form->createView()));
     }
 
     /**
-     * @Route("/book/{id}", name="edit_book")
+     * @Route("/book/edit/{id}", name="edit_book")
+     * Method({"GET", "POST"})
      */
-    public function editBook(int $id): Response {
-      return $this->render('library/edit.html.twig');
+    public function editBook(Request $request, int $id): Response {
+
+      $book = $this->getDoctrine()->getRepository(Book::class)->find($id);
+
+      $form = $this->createFormBuilder($book)
+        ->add('title', TextType::class, array(
+          'label' => 'Название',
+          'attr' => array('class' => 'form-control')))
+        ->add('Author', TextType::class, array(
+          'label' => 'Автор',
+          'attr' => array('class' => 'form-control')))
+        ->add('Year',  TextType::class, array(
+          'label' => 'Год',
+          'attr' => array('class' => 'form-control')))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Сохранить',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+        ))
+        ->getForm();
+
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+        return $this->redirectToRoute('book_list');
+      }
+
+      return $this->render('library/edit.html.twig',
+                            array('form' => $form->createView(),
+                                  'book' => $book->getTitle()));
     }
 
-
-    // /**
-    //  * @Route("/book/save")
-    //  */
-    //  public function save() {
-    //    $entityManager = $this->getDoctrine()->getManager();
-    //
-    //    $book = new Book();
-    //    $book->setTitle("Book 2");
-    //    $book->setAuthor("Author 2");
-    //    $book->setYear(2000);
-    //
-    //    $entityManager->persist($book);
-    //    $entityManager->flush();
-    //
-    //    return new Response(
-    //      'saved the book with the id of '.$book->getId()
-    //    );
-    //  }
 }
 
 
